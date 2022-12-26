e6174b9511b2c892e01b29cb8b3652081753dc4f
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2022-12-26 13:25:35 -0300

    Cambio de iconos de contacto

--- src/components/HomePage/Contact.jsx
+++ src/components/HomePage/Contact.jsx
@@ -14,13 +14,44 @@ const Contact = () => {
 			</p>
 			<div>
 				<a href="https://www.instagram.com/alecalgaro/" target="_blank">
-					<img src={iconInstagram} alt="icono instagram" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="icon icon-tabler icon-tabler-brand-instagram"
+						width="60"
+						height="60"
+						viewBox="0 0 24 24"
+						stroke-width="1.2"
+						stroke="#133757"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<rect x="4" y="4" width="16" height="16" rx="4" />
+						<circle cx="12" cy="12" r="3" />
+						<line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+					</svg>
 				</a>
 				<a
 					href="https://wa.me/+5493456474803?text=Hola!%20Deseo%20adquirir%20un%20taller%20de%20magia"
 					target="_blank"
 				>
-					<img src={iconWhatsapp} alt="icono whatsapp" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="icon icon-tabler icon-tabler-brand-whatsapp"
+						width="60"
+						height="60"
+						viewBox="0 0 24 24"
+						stroke-width="1.2"
+						stroke="#133757"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+						<path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+					</svg>
 				</a>
 			</div>
 		</ContainerContact>
@@ -53,13 +84,12 @@ const ContainerContact = styled.section`
 		margin-top: 2.5rem;
 	}
 
-	img {
-		width: 6rem;
+	svg {
 		cursor: pointer;
 		transition: all 0.3s;
 
 		&:hover {
-			opacity: 0.8;
+			stroke: #3399ff;
 		}
 	}
 `;
