fe5ec612b2ca23bf8b537c52b8a7ac54e4097d04
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-04-03 08:59:16 -0300

    Loader para splash screen

--- src/components/HomePage/SplashScreenApp.jsx
+++ src/components/HomePage/SplashScreenApp.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const SplashScreenApp = () => {
-	return <SplashScreen />;
+	return (
+		<SplashScreen>
+			<span className="loader"></span>
+		</SplashScreen>
+	);
 };
 
 export default SplashScreenApp;
@@ -16,6 +20,36 @@ const SplashScreen = styled.div`
 	left: 0%;
 	height: 100vh;
 	width: 100vw;
-	z-index: 99;
 	background-color: var(--white);
+
+	.loader {
+		padding-right: 6.4rem;
+	}
+
+	.loarder::after,
+	.loader::before {
+		animation: loader 1s linear infinite;
+		content: "";
+		border: 2px solid var(--primary);
+		width: 64px;
+		height: 64px;
+		position: absolute;
+		border-radius: 50%;
+	}
+
+	.loader::after {
+		opacity: 0;
+		animation-delay: 1s;
+	}
+
+	@keyframes loader {
+		0% {
+			transform: scale(0);
+			opacity: 1;
+		}
+		100% {
+			transform: scale(1);
+			opacity: 0;
+		}
+	}
 `;
